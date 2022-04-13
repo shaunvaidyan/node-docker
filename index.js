37edab12d1c32feb6724a3e35b862c712f746330
Shaun Vaidyan <shaun@vaidyan.me>
2022-04-13 12:54:39 -0500

    added middleware auth to fetch, edit, delete posts

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         httpOnly: true,
-        maxAge: 60000,
+        maxAge: 30000,
       },
     })
   );
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,12 +7,12 @@ const router = express.Router()
 
 //localhost:3000/
 router.route("/")
-.get(postController.getAllPosts)
+.get(protect, postController.getAllPosts)
 .post(protect, postController.createPost)
 
 router.route("/:id")
-.get(postController.getOnePost)
-.patch(postController.updatePost)
-.delete(postController.deletePost)
+.get(protect, postController.getOnePost)
+.patch(protect, postController.updatePost)
+.delete(protect, postController.deletePost)
 
 module.exports = router;
